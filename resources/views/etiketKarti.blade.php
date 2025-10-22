80f4d33401576fdf8f72ccaaa2aa33f71fb7bc10
unknown <Administrator@QZU.domainadi.com>
2025-10-22 17:40:15 +0300

    auto commit

--- resources/views/etiketKarti.blade.php
+++ resources/views/etiketKarti.blade.php
@@ -134,7 +134,7 @@ h1 {
       JsBarcode(barcode, barcode.dataset.value, {
         format: "CODE128",
         width: 1.3,
-        height: 30,
+        height: 35,
         displayValue: true,
         background: "#ffffff",
         lineColor: "#343a40",
