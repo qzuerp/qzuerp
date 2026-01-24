038370cb7f55bf86abfa8632c19ba76af55d9056
unknown <Administrator@QZU.domainadi.com>
2026-01-24 17:03:22 +0300

    auto commit

--- resources/views/fason_takibi.blade.php
+++ resources/views/fason_takibi.blade.php
@@ -22,50 +22,67 @@
   
   $tumEvraklar = collect();
   foreach($fasonGiden as $depo) {
-    $evraklar = DB::table($database.'stok10a as s10')
-    ->leftJoin($database.'stok00 as s0', 's10.KOD', '=', 's0.KOD')
-    ->leftJoin($database.'gdef00 as g', 'g.KOD', '=', 's10.AMBCODE')
-    ->leftJoin($database.'stok63t as sevkt', 'sevkt.KOD', '=', 's10.KOD')
-    ->leftJoin($database.'stok63e as sevke', 'sevke.EVRAKNO', '=', 'sevkt.EVRAKNO')
-    ->selectRaw('
-        s10.KOD,
-        s10.STOK_ADI,
-        SUM(s10.SF_MIKTAR) AS SF_MIKTAR,
-        s10.SF_SF_UNIT AS SF_UNIT,
-        s10.LOTNUMBER,
-        s10.SERINO,
-        s10.AMBCODE,
-        g.AD AS DEPO_ADI,
-        s10.TEXT1,
-        s10.TEXT2,
-        s10.TEXT3,
-        s10.TEXT4,
-        s10.NUM1,
-        s10.NUM2,
-        s10.NUM3,
-        s10.NUM4,
-        s10.LOCATION1,
-        s10.LOCATION2,
-        s10.LOCATION3,
-        s10.LOCATION4,
-        s0.NAME2 AS STOK_ADI2,
-        s0.id,
-        s0.REVNO,
-        MAX(sevkt.TERMIN_TAR) AS TERMIN_TAR,
-        MAX(sevke.TARIH) AS TARIH
-    ')
-    ->groupBy(
-        's10.KOD','s10.STOK_ADI','s10.SF_SF_UNIT','s10.LOTNUMBER',
-        's10.SERINO','s10.AMBCODE','g.AD',
-        's10.TEXT1','s10.TEXT2','s10.TEXT3','s10.TEXT4',
-        's10.NUM1','s10.NUM2','s10.NUM3','s10.NUM4',
-        's10.LOCATION1','s10.LOCATION2','s10.LOCATION3','s10.LOCATION4',
-        's0.NAME2','s0.id','s0.REVNO'
-    )
-    ->havingRaw('SUM(s10.SF_MIKTAR) <> 0')
-    ->where('s10.AMBCODE', $depo->KOD)
-    ->get();
-
+      $evraklar = DB::table($database.'stok10a as s10')
+          ->leftJoin($database.'stok00 as s0', 's10.KOD', '=', 's0.KOD')
+          ->leftJoin($database.'gdef00 as g', 'g.KOD', '=', 's10.AMBCODE')
+          ->leftJoin($database.'stok63t as sevkt', 'sevkt.KOD', '=', 's10.KOD')
+          ->selectRaw('
+              s10.KOD,
+              s10.STOK_ADI,
+              SUM(s10.SF_MIKTAR) AS SF_MIKTAR,
+              s10.SF_SF_UNIT AS SF_UNIT,
+              s10.LOTNUMBER,
+              s10.SERINO,
+              s10.AMBCODE,
+              g.AD AS DEPO_ADI,
+              s10.TEXT1,
+              s10.TEXT2,
+              s10.TEXT3,
+              s10.TEXT4,
+              s10.NUM1,
+              s10.NUM2,
+              s10.NUM3,
+              s10.NUM4,
+              s10.LOCATION1,
+              s10.LOCATION2,
+              s10.LOCATION3,
+              s10.LOCATION4,
+              s0.NAME2 AS STOK_ADI2,
+              s0.id,
+              s0.REVNO,
+              sevkt.TERMIN_TAR,
+              sevkt.created_at
+          ')
+          ->groupBy(
+            's10.KOD',
+            's10.STOK_ADI',
+            's10.SF_MIKTAR',
+            's10.SF_SF_UNIT',
+            's10.LOTNUMBER',
+            's10.SERINO',
+            's10.AMBCODE',
+            'g.AD',
+            's10.TEXT1',
+            's10.TEXT2',
+            's10.TEXT3',
+            's10.TEXT4',
+            's10.NUM1',
+            's10.NUM2',
+            's10.NUM3',
+            's10.NUM4',
+            's10.LOCATION1',
+            's10.LOCATION2',
+            's10.LOCATION3',
+            's10.LOCATION4',
+            's0.NAME2',
+            's0.id',
+            's0.REVNO',
+            'sevkt.TERMIN_TAR',
+            'sevkt.created_at'
+            )
+          ->havingRaw('SUM(s10.SF_MIKTAR) <> 0')
+          ->where('s10.AMBCODE','=',$depo->KOD)
+          ->get();
       
       $tumEvraklar = $tumEvraklar->merge($evraklar);
   }
@@ -258,7 +275,7 @@
                                             <td>{{ $item->STOK_ADI2 }}</td>
                                             <td>{{ $item->REVNO }}</td>
                                             <td>{{ number_format($item->SF_MIKTAR, 2, ',', '.') }}</td>
-                                            <td>{{ $item->TARIH }}</td>
+                                            <td>{{ $item->created_at }}</td>
                                             <td>{{ $item->TERMIN_TAR }}</td>
                                             <td>{{ $item->SF_UNIT }}</td>
                                             <td>{{ $item->LOTNUMBER }}</td>
