1da22d4f16eae5287514845d0cf42be0e132b9f1
unknown <Administrator@QZU.domainadi.com>
2026-04-03 09:16:40 +0300

    auto commit

---
+++ database/migrations/2026_03_24_102738_stok_trigger.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class StokTrigger extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared("
+            CREATE TRIGGER trg_stok10a_PreventDelete
+            ON stok10a
+            INSTEAD OF DELETE
+            AS
+            BEGIN
+                SET NOCOUNT ON;
+
+                IF EXISTS (
+                    SELECT 1 
+                    FROM stok68t s68
+                    INNER JOIN deleted d ON s68.EVRAKNO = d.EVRAKNO AND s68.TRNUM = d.TRNUM AND d.EVRAKTYPE LIKE '%STOK68T&'
+                )
+                BEGIN
+                    RAISERROR ('Bu kayıt stok68t tablosunda kullanılıyor, silemezsin', 16, 1);
+                    ROLLBACK TRANSACTION;
+                END
+                ELSE
+                BEGIN
+                    DELETE s10
+                    FROM stok10a s10
+                    INNER JOIN deleted d ON s10.id = d.id;
+                END
+            END
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
