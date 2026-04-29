b0bb631e804425d25539f5f5530cf9e2549300f0
unknown <Administrator@QZU.domainadi.com>
2026-04-29 16:32:54 +0300

    auto commit

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -51,8 +51,8 @@ class NotificationController extends Controller
         
         $durum = true;
         $name = '';
-        
-        DB::statement("DELETE FROM D7WLOCK WHERE ACTIVE_TIME < DATEADD(second, -45, GETDATE())");
+
+        DB::statement("DELETE FROM ".$firma."D7WLOCK WHERE ACTIVE_TIME < DATEADD(second, -45, GETDATE())");
 
         $salt = DB::table($firma.'D7WLOCK')->where('EVRAKNO', $EVRAKNO)->where('EVRAKTYPE', $EVRAKTYPE)->first();
 
