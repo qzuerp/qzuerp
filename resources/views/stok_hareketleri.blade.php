542c152f1e5e5cd74796d93c5082a1ef0f47c91e
unknown <Administrator@QZU.domainadi.com>
2026-06-22 17:55:11 +0300

    auto commit

--- resources/views/stok_hareketleri.blade.php
+++ resources/views/stok_hareketleri.blade.php
@@ -414,7 +414,7 @@ $kullanici_delete_yetkileri = explode("|", $kullanici_veri->delete_perm);
 			dt.rows().every(function () {
 				let d = this.data();
 				data.push([
-					d.created_at ?? '',
+					d.created_at ? new Date(d.created_at) : '',
 					d.KOD ?? '',
 					d.STOK_ADI ?? '',
 					d.NAME2 ?? '',
